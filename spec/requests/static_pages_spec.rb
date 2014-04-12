342b8cf11d47ac9b063fd3f0032d2a94599f12a0
Petr Skocik <pskocik@gmail.com>
2014-04-12 12:22:52 +0200

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,10 @@ describe "Static Pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
   end
 
   describe "Help Page" do
@@ -13,12 +17,20 @@ describe "Static Pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
   end
 
   describe "About Page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
     end
   end
 end
