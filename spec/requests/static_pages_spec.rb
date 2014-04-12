05508147f8939ba7f40bf2dbbafed3a028e9da4c
Petr Skocik <pskocik@gmail.com>
2014-04-12 12:32:13 +0200

    extract base_title into a variable

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   describe "Home Page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -8,7 +9,7 @@ describe "Static Pages" do
     end
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -19,7 +20,7 @@ describe "Static Pages" do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -30,7 +31,7 @@ describe "Static Pages" do
     end
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 end
