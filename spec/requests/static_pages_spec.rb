8eefce7735dc395d9430bf7891bbf536b91f5a89
Petr Skocik <pskocik@gmail.com>
2014-04-13 14:13:03 +0200

    use new routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,38 +4,49 @@ describe "Static Pages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   describe "Home Page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit home_path
       expect(page).to have_content('Sample App')
     end
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit home_path
       expect(page).to have_title("#{base_title}")
     end
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit home_path
       expect(page).to_not have_title("| Home")
     end
   end
 
   describe "Help Page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe "About Page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About")
+    end
+  end
+
+  describe "Contact Page" do
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_selector('h1', text: 'Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
