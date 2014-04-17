21d66c2a16c6ba3edcb6d86dacfc7ac64f6c73dd
Petr Skocik <pskocik@gmail.com>
2014-04-17 02:11:14 +0200

    finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -31,5 +31,13 @@ module SessionsHelper
                    User.hash(cookies[:remember_token]))
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
 
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,13 @@
 
 FactoryGirl.define do
   factory :user do
-    name "Petr Skocik"
-    email "pskocik@gmail.com"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person#{n}@example.com" }
     password "foobar"
     password_confirmation "foobar"
+
+      factory :admin do
+        admin true
+      end
   end
 end
