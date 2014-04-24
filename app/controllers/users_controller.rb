f4cb9c84d88ca033cfebd9a410b05c0c91d3a958
Petr Skocik <pskocik@gmail.com>
2014-04-24 13:40:34 +0200

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
@@ -53,12 +54,6 @@ class UsersController < ApplicationController
     end
 
     #Before actions
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in." 
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,17 @@ class User < ActiveRecord::Base
   before_create :create_remember_token
   before_save { self.email.downcase! }
   has_secure_password
+  has_many :microposts, dependent: :destroy
   validates :password, length: { minimum: 6 }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
+
+  def feed
+    #microposts
+    Micropost.where("user_id = ?", id)
+  end
 
   #Public class methods
   class << self
